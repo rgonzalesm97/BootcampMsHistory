80d14403e590c61a4c7cfe82f5034cab812c8be5
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2022-02-14 20:50:28 -0500

    update history no mayor changes

--- src/main/java/com/bank/history/model/dao/Client.java
+++ src/main/java/com/bank/history/model/dao/Client.java
@@ -1,15 +1,11 @@
 package com.bank.history.model.dao;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
 @Data
-@Document("client")
 public class Client {
 
-	@Id
 	private String id;
 	private String name;
 	private String type;
